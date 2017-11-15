0aee350b29fa6c9023addf968d1dcbc2a2270737
Nina Queuche <n.queuche@gmail.com>
2017-11-15 20:23:49 +0100

    cocoapods install + storyb

---
+++ StarPedia/Controllers/PlanetDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PlanetDetailViewController.swift
+//  StarPedia
+//
+//  Created by Nina Queuche on 15/11/2017.
+//  Copyright © 2017 WSF. All rights reserved.
+//
+
+
+import UIKit
+import Kingfisher
+
+class PlanetDetailViewController: UIViewController {
+    
+}
---
+++ StarPedia/Controllers/PlanetsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetsViewController.swift
+//  StarPedia
+//
+//  Created by Nina Queuche on 15/11/2017.
+//  Copyright © 2017 WSF. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import RealmSwift
+
+class PlanetsViewController : UIViewController {
+
+    
+}
