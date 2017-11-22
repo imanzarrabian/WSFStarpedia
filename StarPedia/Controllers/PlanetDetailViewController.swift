0967d86595dc301d6d60438914a2f4a28b338bab
lpaicheur <lpaicheur@users.noreply.github.com>
2017-11-22 10:04:44 +0100

    Add labels for planets

--- StarPedia/Controllers/PlanetDetailViewController.swift
+++ StarPedia/Controllers/PlanetDetailViewController.swift
@@ -12,12 +12,17 @@ import Kingfisher
 class PlanetDetailViewController: UIViewController {
     
     @IBOutlet weak var pictureIV: UIImageView!
+    @IBOutlet weak var rotationPeriodLabel: UILabel!
+    @IBOutlet weak var orbitalPerdiodLabel: UILabel!
     
     var planet: Planet!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = planet.name
+        
+        rotationPeriodLabel.text = planet.rotation_period + " days"
+        orbitalPerdiodLabel.text = planet.orbital_period + " hours"
 
         let url = URL(string: planet.pictureURL)
                 pictureIV.kf.setImage(with: url)
